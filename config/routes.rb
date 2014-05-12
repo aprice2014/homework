dab6ff5c321c7542ffb02c66e3b793418736b7cf
Alexander Price <aprice2014@kellogg.northwestern.edu>
2014-05-12 05:41:53 +0000

    first attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Homework::Application.routes.draw do
  get "/dice/roll"
 
- get "books/list"
+ get "books" => "books/list"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422004225) do
+ActiveRecord::Schema.define(version: 20140512031403) do
 
   create_table "books", force: true do |t|
-    t.string "title"
-    t.string "author"
+    t.string  "author"
+    t.string  "book_title"
+    t.integer "price"
+    t.integer "review_id"
+    t.integer "book_id"
+  end
+
+  create_table "ratings", force: true do |t|
+    t.integer "star_rating"
+    t.string  "review"
   end
 
 end
